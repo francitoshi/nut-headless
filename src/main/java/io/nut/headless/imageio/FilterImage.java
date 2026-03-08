1f1ed970d97463696e50da148e9c4a95454d33b9
francitoshi <francitoshi@gmail.com>
2026-03-08 20:26:36 +0100

    fix headers

--- src/main/java/io/nut/headless/imageio/FilterImage.java
+++ src/main/java/io/nut/headless/imageio/FilterImage.java
@@ -1,7 +1,7 @@
 /*
  *  ScaleImage.java
  *
- *  Copyright (C) 2009-2011  Francisco Gómez Carrasco
+ *  Copyright (C) 2009-2026 francitoshi@gmail.com
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  Report bugs or new features to: flikxxi@gmail.com
+ *  Report bugs or new features to: francitoshi@gmail.com
  *
  */
 package io.nut.headless.imageio;
