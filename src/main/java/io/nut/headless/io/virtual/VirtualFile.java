c7fee9c7f8a387331917e8d0f1734923ef14a1f3
francitoshi <francitoshi@gmail.com>
2026-08-24 12:03:28 +0200

    fix bug in VirtualFile.getAbsoluteFile()

--- src/main/java/io/nut/headless/io/virtual/VirtualFile.java
+++ src/main/java/io/nut/headless/io/virtual/VirtualFile.java
@@ -1,22 +1,7 @@
 /*
- *  VirtualFile.java
- *
- *  Copyright (C) 2010-2024 francitoshi@gmail.com
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Report bugs or new features to: francitoshi@gmail.com
+ * Copyright (C) 2010-2026 francitoshi@gmail.com
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * See LICENSE file in the project root for full license text.
  */
 package io.nut.headless.io.virtual;
 
@@ -95,7 +80,7 @@ public class VirtualFile implements Comparable<VirtualFile>, Cloneable
     public VirtualFile getAbsoluteFile() throws IOException
     {
         VirtualFileSystem af = fs.getAbsoluteFile();
-        if(af!=af)
+        if(fs!=af)
         {
             return new VirtualFile(af);
         }
