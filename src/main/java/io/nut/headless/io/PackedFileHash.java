ab9fbee4b9674bac2cd58d40b11fbe7cb3d32d18
francitoshi <francitoshi@gmail.com>
2026-08-24 12:36:14 +0200

    fix bug on exception in PackedFileHash.equals()

--- src/main/java/io/nut/headless/io/PackedFileHash.java
+++ src/main/java/io/nut/headless/io/PackedFileHash.java
@@ -1,22 +1,7 @@
 /*
- *  PackedFileHash.java
- *
- *  Copyright (C) 2007-2024 francitoshi@gmail.com
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Report bugs or new features to: francitoshi@gmail.com
+ * Copyright (C) 2007-2026 francitoshi@gmail.com
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * See LICENSE file in the project root for full license text.
  */
 package io.nut.headless.io;
 
@@ -130,11 +115,14 @@ public class PackedFileHash
         }
         catch (ArchiveException ex)
         {
-            Logger.getLogger(PackedFileHash.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(PackedFileHash.class.getName()).log(Level.WARNING, null, ex);
+            exception = true;
+            return false;
         }
         catch (IOException ex)
         {
             Logger.getLogger(PackedFileHash.class.getName()).log(Level.SEVERE, null, ex);
+            exception = true;
             return false;
         }
         return true;
